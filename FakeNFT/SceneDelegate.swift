c4e11ae759ef815312fb1cf15c3ba173ef787004
MaximKakoulin <mximm1213@gmail.com>
2023-12-17 22:51:58 +0700

    Added SceneDelegate

--- FakeNFT/SceneDelegate.swift
+++ FakeNFT/SceneDelegate.swift
@@ -3,13 +3,18 @@ import UIKit
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
-    let servicesAssembly = ServicesAssembly(
-        networkClient: DefaultNetworkClient(),
-        nftStorage: NftStorageImpl()
-    )
-
-    func scene(_: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
-        let tabBarController = window?.rootViewController as? TabBarController
-        tabBarController?.servicesAssembly = servicesAssembly
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let appConfig = AppConfiguration()
+        let tabBarController = TabBarController(appConfiguration: appConfig)
+        window.rootViewController = tabBarController
+        self.window = window
+        window.makeKeyAndVisible()
     }
 }
+
