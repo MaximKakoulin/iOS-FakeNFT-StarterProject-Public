89a10c2ada8f66cc26a3c8118e79af908aa5bed1
MaximKakoulin <mximm1213@gmail.com>
2023-12-17 22:50:57 +0700

    Added TabBar

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -1,25 +1,65 @@
 import UIKit
 
-final class TabBarController: UITabBarController {
+class TabBarController: UITabBarController {
 
-    var servicesAssembly: ServicesAssembly!
+    let appConfiguration: AppConfiguration
 
-    private let catalogTabBarItem = UITabBarItem(
-        title: NSLocalizedString("Tab.catalog", comment: ""),
-        image: UIImage(systemName: "square.stack.3d.up.fill"),
-        tag: 0
-    )
+    init(appConfiguration: AppConfiguration) {
+        self.appConfiguration = appConfiguration
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        // Установите значения по умолчанию или инициализируйте appConfiguration здесь
+        self.appConfiguration = AppConfiguration() // Пример, замените на реальную инициализацию
+        super.init(coder: coder)
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let catalogController = TestCatalogViewController(
-            servicesAssembly: servicesAssembly
+        let profileNavigationController = UINavigationController(rootViewController: appConfiguration.profileViewController)
+        profileNavigationController.tabBarItem = UITabBarItem(
+            title: S.TabBarController.profileTabBarTitle,
+            image: UIImage(systemName: "person.crop.circle.fill"),
+            selectedImage: nil
         )
-        catalogController.tabBarItem = catalogTabBarItem
 
-        viewControllers = [catalogController]
+        appConfiguration.catalogViewController.tabBarItem = UITabBarItem(
+            title: S.TabBarController.catalogTabBarTitle,
+            image: UIImage(systemName: "rectangle.stack.fill"),
+            selectedImage: nil
+        )
 
-        view.backgroundColor = .systemBackground
+        appConfiguration.cartViewController.tabBarItem = UITabBarItem(
+            title: S.TabBarController.cartTabBarTitle,
+            image: UIImage(systemName: "bag.fill"),
+            selectedImage: nil
+        )
+        appConfiguration.statisticViewController.tabBarItem = UITabBarItem(
+            title: S.TabBarController.statisticTabBarTitle,
+            image: UIImage(systemName: "flag.2.crossed.fill"),
+            selectedImage: nil
+        )
+
+        self.viewControllers = [
+            //appConfiguration.profileViewController,
+            profileNavigationController,
+            appConfiguration.catalogViewController,
+            appConfiguration.cartViewController,
+            appConfiguration.statisticViewController
+        ]
+
+        tabBar.isTranslucent = false
+        view.tintColor = .systemBlue
+        tabBar.backgroundColor = .white
+
+        if #available(iOS 15.0, *) {
+            let appearance = UITabBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = .white
+            tabBar.standardAppearance = appearance
+            tabBar.scrollEdgeAppearance = appearance
+        }
     }
 }
