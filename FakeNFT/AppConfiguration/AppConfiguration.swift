2ddebab90f24ca065c72afba3092d5c4abd776f7
MaximKakoulin <mximm1213@gmail.com>
2023-12-17 22:49:11 +0700

    Added AppConfiguration

---
+++ FakeNFT/AppConfiguration/AppConfiguration.swift
@@ -0,0 +1,28 @@
+//
+//  AppConfiguration.swift
+//  FakeNFT
+//
+//  Created by Максим on 17.12.2023.
+//
+
+import UIKit
+
+class AppConfiguration {
+    let profileViewController: UIViewController
+    let catalogViewController: UIViewController
+    let cartViewController: UIViewController
+    let statisticViewController: UIViewController
+
+    init() {
+        //MARK: - Эпик Профиля
+        let networkClient = DefaultNetworkClient()
+        let profileService = ProfileService(networkClient: networkClient)
+        let profilePresenter = ProfilePresenter(view: nil, profileService: profileService) // Временно установим view в nil
+        profileViewController = ProfileViewController(presenter: profilePresenter)
+        profilePresenter.view = profileViewController as? ProfileViewProtocol // Теперь устанавливаем view
+
+        catalogViewController = UIViewController()
+        cartViewController = UIViewController()
+        statisticViewController = UIViewController()
+    }
+}
