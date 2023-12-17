30c1428032be801166b92b07c51ffe3525800b9b
MaximKakoulin <mximm1213@gmail.com>
2023-12-17 22:53:42 +0700

    Added ProfilePresenter

---
+++ FakeNFT/Scenes /Profile/ProfileVCPresenter.swift
@@ -0,0 +1,59 @@
+//
+//  ProfileVCPresenter.swift
+//  FakeNFT
+//
+//  Created by Максим on 17.12.2023.
+//
+
+import Foundation
+
+protocol ProfilePresenterProtocol: AnyObject {
+    func viewDidLoad()
+    func didTapEditProfile()
+    func didTapMyNFTs()
+    func didTapFavorites()
+    func didTapAboutDeveloper()
+}
+
+class ProfilePresenter: ProfilePresenterProtocol {
+
+    weak var view: ProfileViewProtocol?
+
+    var profileService: ProfileServiceProtocol
+
+    init(view: ProfileViewProtocol?, profileService: ProfileServiceProtocol) {
+        self.view = view
+        self.profileService = profileService
+    }
+
+    func viewDidLoad() {
+        profileService.fetchUserProfile { [weak self] result in
+            switch result {
+            case .success(let profile):
+                DispatchQueue.main.async {
+                    self?.view?.updateUI(with: profile)
+                }
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self?.view?.displayError(error)
+                }
+            }
+        }
+    }
+
+    func didTapEditProfile() {
+        view?.navigateToEditProfileScreen()
+    }
+
+    func didTapMyNFTs() {
+
+    }
+
+    func didTapFavorites() {
+
+    }
+
+    func didTapAboutDeveloper() {
+
+    }
+}
