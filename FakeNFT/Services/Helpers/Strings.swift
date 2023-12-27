f69180219d17e994f3662be5316d49058c7afc31
MaximKakoulin <mximm1213@gmail.com>
2023-12-27 19:15:27 +0700

    Added NSLocalizedString

--- FakeNFT/Services/Helpers/Strings.swift
+++ FakeNFT/Services/Helpers/Strings.swift
@@ -9,38 +9,146 @@ import Foundation
 
 struct TextLabels {
     struct TabBarController {
-        static let profileTabBarTitle = "Профиль"
-        static let catalogTabBarTitle = "Каталог"
-        static let cartTabBarTitle = "Корзина"
-        static let statisticTabBarTitle = "Статистика"
+        static var profileTabBarTitle: String {
+            return NSLocalizedString("profileTabBarTitle", comment: "")
+        }
+        static var catalogTabBarTitle: String {
+            return NSLocalizedString("catalogTabBarTitle", comment: "")
+        }
+        static var cartTabBarTitle: String {
+            return NSLocalizedString("cartTabBarTitle", comment: "")
+        }
+        static var statisticTabBarTitle: String {
+            return NSLocalizedString("statisticTabBarTitle", comment: "")
+        }
     }
 
     struct ProfileEditVC {
-        static let avatarLabel: String = "Cменить\nфото"
-        static let nameStackViewLabel: String = "Имя"
-        static let nameStackViewContent: String = "Введите Ваше имя"
-        static let descriptionStackViewLabel: String = "Описание"
-        static let descriptionStackViewContent: String = "Введите Ваше описание"
-        static let websiteStackViewLabel: String = "Сайт"
-        static let websiteStackViewContent: String = "Введите Ваш Сайт"
-        static let profileUpdatedSuccessfully: String = "Профиль успешно обновлен"
-        static let saveButton: String = "Сохранить"
+        static var avatarLabel: String {
+            return NSLocalizedString("avatarLabel", comment: "")
+        }
+        static var nameStackViewLabel: String {
+            return NSLocalizedString("nameStackViewLabel", comment: "")
+        }
+        static var nameStackViewContent: String {
+            return NSLocalizedString("nameStackViewContent", comment: "")
+        }
+        static var descriptionStackViewLabel: String {
+            return NSLocalizedString("descriptionStackViewLabel", comment: "")
+        }
+        static var descriptionStackViewContent: String {
+            return NSLocalizedString("descriptionStackViewContent", comment: "")
+        }
+        static var websiteStackViewLabel: String {
+            return NSLocalizedString("websiteStackViewLabel", comment: "")
+        }
+        static var websiteStackViewContent: String {
+            return NSLocalizedString("websiteStackViewContent", comment: "")
+        }
+        static var profileUpdatedSuccesfully: String {
+            return NSLocalizedString("profileUpdatedSuccessfully", comment: "")
+        }
+        static var saveButton: String {
+            return NSLocalizedString("saveButton", comment: "")
+        }
     }
 
     struct ProfileEditStackView {
-        static let keyboardDoneButton: String = "Готово"
-        static let keyboardResetButton: String = "Отменить ввод"
+        static var keyboardDoneButton: String {
+            return NSLocalizedString("keyboardDoneButton", comment: "")
+        }
+        static var keyboardResetButton: String {
+            return NSLocalizedString("keyboardResetButton", comment: "")
+        }
     }
 
     struct ProfileButtonsStackView {
-        static let myNFTLabel: String = "Мои NFT"
-        static let favoritesNFTLabel: String = "Избранные NFT"
-        static let aboutDeveloperLabel: String = "О разработчике"
+        static var myNFTLabel: String {
+            return NSLocalizedString("myNFTLabel", comment: "")
+        }
+        static var favoritesNFTLabel: String {
+            return NSLocalizedString("favoritesNFTLabel", comment: "")
+        }
+        static var aboutDeveloperLabel: String {
+            return NSLocalizedString("aboutDeveloperLabel", comment: "")
+        }
     }
 
     struct UserProfileStackView {
-        static let userNameLabel: String = "Имя Пользователя"
-        static let userInfoLabel: String = "Информация о Вас. Здесь может быть ваши данные или другую информацию."
-        static let websiteLabel: String = "www.internet.com"
+        static var userNameLabel: String {
+            return NSLocalizedString("userNameLabel", comment: "")
+        }
+        static var userInfoLabel: String {
+            return NSLocalizedString("userInfoLabel", comment: "")
+        }
+        static var websiteLabel: String {
+            return NSLocalizedString("websiteLabel", comment: "")
+        }
+    }
+
+    struct MyNFTsVC {
+        static var navigationTitle: String {
+            return NSLocalizedString("myNFTsNavigationTitle", comment: "")
+        }
+        static var cellNFTPriceLabel: String {
+            return NSLocalizedString("cellNFTPriceLabel", comment: "")
+        }
+        static var alertTitleLabel: String {
+            return NSLocalizedString("alertTitleLabel", comment: "")
+        }
+        static var alertPriceLabel: String {
+            return NSLocalizedString("alertPriceLabel", comment: "")
+        }
+        static var alertRatingLabel: String {
+            return NSLocalizedString("alertRatingLabel", comment: "")
+        }
+        static var alertNameLabel: String {
+            return NSLocalizedString("alertNameLabel", comment: "")
+        }
+        static var alertCloseLabel: String {
+            return NSLocalizedString("alertCloseLabel", comment: "")
+        }
+        static var placeholder: String {
+            return NSLocalizedString("myNFTsPlaceholder", comment: "")
+        }
+    }
+
+    struct FavoritesNFTsVC {
+        static var navigationTitle: String {
+            return NSLocalizedString("favoritesNFTsNavigationTitle", comment: "")
+        }
+        static var placeholder: String {
+            return NSLocalizedString("favoritesNFTsPlaceholder", comment: "")
+        }
+    }
+
+    struct AboutDevelopersVC {
+        static var navigationTitle: String {
+            return NSLocalizedString("aboutDevelopersNavigationTitle", comment: "")
+        }
+    }
+
+    struct OnboardingVC {
+        static var firstHeader: String {
+            return NSLocalizedString("firstHeader", comment: "")
+        }
+        static var firstDescription: String {
+            return NSLocalizedString("firstDescription", comment: "")
+        }
+        static var secondHeader: String {
+            return NSLocalizedString("secondHeader", comment: "")
+        }
+        static var secondDescription: String {
+            return NSLocalizedString("secondDescription", comment: "")
+        }
+        static var thirdHeader: String {
+            return NSLocalizedString("thirdHeader", comment: "")
+        }
+        static var thirdDescription: String {
+            return NSLocalizedString("thirdDescription", comment: "")
+        }
+        static var onboardingButton: String {
+            return NSLocalizedString("onboardingButton", comment: "")
+        }
     }
 }
